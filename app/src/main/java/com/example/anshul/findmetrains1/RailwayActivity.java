b36656f0d9509ddd8122573c564345d30f546a20
Anshul-Panwar <anshul.s.panwar@gmail.com>
2015-11-23 22:09:52 +0530

    Updated Railway activity. Json parsing.

--- app/src/main/java/com/example/anshul/findmetrains1/RailwayActivity.java
+++ app/src/main/java/com/example/anshul/findmetrains1/RailwayActivity.java
@@ -2,19 +2,59 @@ package com.example.anshul.findmetrains1;
   import android.os.Bundle;
 import android.webkit.WebView;
 import android.app.*;
+  import android.widget.TextView;
 
-import java.io.IOException;
+  import java.io.BufferedReader;
+  import java.io.IOException;
+  import java.io.InputStream;
+  import java.io.InputStreamReader;
+  import java.net.HttpURLConnection;
+  import java.net.MalformedURLException;
+  import java.net.URL;
 
-   public class RailwayActivity extends MapsActivity  {
-
+public class RailwayActivity extends MapsActivity  {
+TextView tv;
        @Override
        protected void onCreate(Bundle savedInstanceState) {
            super.onCreate(savedInstanceState);
-           WebView webview = new WebView(this);
-           setContentView(webview);
+           setContentView(R.layout.railwaylayout);
            Bundle b=this.getIntent().getExtras();
-           String[] array=b.getStringArray("key");
-           webview.loadUrl("https://maps.googleapis.com/maps/api/directions/json?origin=\"+array[0]+\"&destination=\"+array[1]+\"&transit_mode=train&key= \n");
+           String array[]=b.getStringArray("key");
+           HttpURLConnection connection = null;
+           BufferedReader reader = null;
+           try {
+               URL url = new URL("https://maps.googleapis.com/maps/api/directions/json?origin=\"+array[0]+\"&destination=\"+array[1]+\"&transit_mode=train&key=");
+               connection = (HttpURLConnection) url.openConnection();
+               connection.connect();
+
+               InputStream stream = connection.getInputStream();
+               reader = new BufferedReader(new InputStreamReader(stream));
+               StringBuffer buffer = new StringBuffer();
+               String line = "";
+               while ((line = reader.readLine()) != null) {
+                   buffer.append(line);
+               }
+
+               tv.setText(buffer.toString());
+           } catch (MalformedURLException e) {
+               e.printStackTrace();
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+           finally {
+               if(connection!=null) {
+                   connection.disconnect();
+               }
+               try {
+                   if(reader!=null) {
+                       reader.close();
+                   }
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+
+           }
+
 
        }
    }
